222e5d45f4f39e10ed104d6a3302566f60723e01
gautham-prasad <gautham.prasadg@gmail.com>
2021-09-19 22:54:32 +0530

    update:

--- app.py
+++ app.py
@@ -106,8 +106,10 @@ def verify(token):
 
     if email == temp_user.email and temp_user.verified == False:
 
-            temp_user.verified = True
+            tempuser = Tempusers(verified = True)
+            db.session.add(tempuser)
             db.session.commit()
+
 
             user = Users(email = temp_user.email, username = temp_user.username, password = temp_user.password)
             db.session.add(user)
